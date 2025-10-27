dd2ef8ffbf931c9cdff401ff6f4bcc4ac4ee34e9
Yasmim-F <2024003745@ifam.edu.br>
2025-10-27 19:57:43 -0400

    Classe pessoa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Ola mundo")
+class Pessoa{
+    constructor(nome, email){
+        this.nome=nome
+        this.email= email
+    }
+
+    apresentar(){
+        return`Ola, eu soi ${this.nome} e meu email é ${this.email}`;
+    }
+}
+
+let pessoaExemplo= new Pessoa("Yasmim", "email@gmail.com")
+
+console.log(pessoaExemplo.apresentar())
